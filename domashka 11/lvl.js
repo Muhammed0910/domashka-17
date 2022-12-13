27f0cc423f0a0d32599ead63a1c6fa1bb69af4cb
Muhammed0910 <115030918+Muhammed0910@users.noreply.github.com>
2022-12-13 21:51:09 +0400

    Add files via upload

---
+++ domashka 11/lvl.js
@@ -0,0 +1,16 @@
+//lvl1
+function boomlakalaka(n){
+if(n = n) return n    
+}
+console.log(boomlakalaka)
+//lvl2 
+a = jou
+b = jon
+function concat(a, b){
+return (a + ',' + b)
+}
+//lvl3 dont understand
+//lvl4
+console.log(eval(2 + 2))
+console.log(eval(3 / 2))
+//lvl5 dont understand
---
+++ domashka 11/test.js
@@ -0,0 +1,114 @@
+1)
+//перечисли все типы данных
+Null
+Undefined
+Числа
+Текстовые строки
+Символ
+Большое целое
+2)
+//перечисли типы объектов
+null
+undefined
+boolean
+number
+string
+object
+symbol
+3)
+//какие типы данных не могут содержать значения
+null
+undefined
+boolean
+symbol
+4)
+//выведите в консоли тип данных ниже
+let a = 'John'; //object
+let b = 3.14; //number
+let c = NaN; //udefined
+let d = false; //undefined
+let e = [1, 2, 3, 4]; //object
+let f = {name: 'ALex', age: 36}; //string
+let q = new Date(); //boolean
+let r = function () {}; //boolean
+let k; //?
+let n = null; //null
+alert(typeof(a))
+alert(typeof(b))
+alert(typeof(c))
+alert(typeof(d))
+alert(typeof(e))
+alert(typeof(f))
+alert(typeof(q))
+alert(typeof(r))
+alert(typeof(k))
+alert(typeof(n))
+5)
+//преобразуйте строки ниже в числа c помощью Number
+let a = '23.5';
+a = Number(a);
+let b = '20';
+b = Number(b);
+let d = '0000000.1';
+d = Number(d);
+let c = ' ';
+c = Number(c);
+let n = 'hello';
+n = Number(n);
+6)
+//преобразуйте строки ниже в числа c помощью унарного плюса
+let +a = '23.5';
+let +b = '20';
+let +d = '0000000.1';
+let +c = ' ';
+let +n = 'hello';
+7)
+//вырежте из строк ниже целую часть числа и выведите в консоли
+let a = '23.5px';
+let b = '20rem';
+let d = '0000000.1deg';
+?
+8)
+//вырежте из строк ниже числа вместе с плавающей точкой и выведите в консоли
+let a = '23.5px';
+let b = '20rem';
+let d = '0000000.1deg';
+?
+9)
+//преобразуйте переменные ниже в строки с помощью String
+let a = 234;
+a = String(a)
+let b = true;
+b = String(b)
+alert(typeof(a))
+alert(typeof(b))
+10)
+//преобразуйте переменные ниже в строки с помощью toString
+let a = 234;
+let b = true;
+a = a.toString
+b = b.toString
+11)
+//преобразуйте дату в число и выведите в консоли
+let date = new Date();
+date = Number(date)
+12)
+//что выведет в консоли 
+let a = 5 + null;
+//5
+13)
+//что выведет в консоли 
+let b = '5' + null;
+//5null
+14)
+//что выведет в консоли 
+let c = '5' + 2;
+'7'
+15)
+//что выведет в консоли 
+let d = '5' - 2;
+'3'
+16) 
+//что выведет в консоли 
+let r = '5' * 2;
+'5', '5'
